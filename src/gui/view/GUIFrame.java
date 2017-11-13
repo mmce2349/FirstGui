2f90163684ded47a10f39c8ef3742a3e2b0346ce
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2017-11-13 08:29:21 -0700

    Made the basic frame for gui.

--- src/gui/view/GUIFrame.java
+++ src/gui/view/GUIFrame.java
@@ -7,17 +7,19 @@ import javax.swing.JFrame;
 public class GUIFrame extends JFrame
 {
 	private GUIAppController appController;
+	private GUIPanel firstPanel;
 	
 	public GUIFrame(GUIAppController appController)
 	{
 		super();
 		this.appController = appController;
-		
+		this.firstPanel = new GUIPanel(appController);
 		setupFrame();
 	}
 	
 	private void setupFrame()
 	{
+		this.setContentPane(firstPanel);
 		this.setTitle("The Window Title");
 		this.setSize(500, 500);
 		this.setVisible(true);//must be last line
